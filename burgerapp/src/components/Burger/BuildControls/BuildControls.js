541747c61b310a545a46733091c217093d48ff64
Nguyen Thanh Nam <thanhnam192@gmail.com>
2020-05-30 20:40:51 +0700

    Build Controls
    Build Controls

---
+++ burgerapp/src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+    {label : 'Salad', value : 'salad'},
+    {label : 'Meat', value : 'meat'},
+    {label : 'Bacon', value : 'bacon'},
+    {label : 'Cheese', value : 'cheese'},
+];
+
+const buildControls = (props) => {
+    return (
+
+        <div className={classes.BuildControls}>
+            <p>Current price(USD): <strong>{props.price.toFixed(2)}</strong></p>
+            { controls.map( control => (
+            <BuildControl 
+                key={control.label} 
+                label={control.label}
+                added={ () => props.ingredientAdded(control.value) }
+                removed={ () => props.ingredientRemoved(control.value)}
+                disabled={props.disabled[control.value]}
+                />
+                
+            
+            ) )}
+            <button disabled={!props.purchaseable} className={classes.OrderButton}>ORDER NOW</button>
+        </div>
+    );
+}
+
+export default buildControls;
--- burgerapp/src/components/Burger/Burger.js
+++ burgerapp/src/components/Burger/Burger.js
@@ -3,14 +3,20 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
             .map( igKey => {
                 return [...Array(props.ingredients[igKey])].map( (_,index) => {
                     return <BurgerIngredient key={igKey + index} type={igKey} />
                 } );                
-            });
+            })
+            .reduce( (arr,el) => { 
+                return arr.concat(el);
+            },[]);
 
             console.log(transformedIngredients);
+            if ( transformedIngredients.length === 0 ){
+                transformedIngredients = <p>Please start adding ingredients!</p>;
+            }
 
     return (
         <div className={classes.Burger}>
