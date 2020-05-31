38a43a93b85a19d02070a92d9e3ff32ed2458607
Nguyen Thanh Nam <thanhnam192@gmail.com>
2020-05-31 14:54:23 +0700

    change folder structure
    change folder structure

--- burgerapp/src/components/Burger/OrderSummary/OrderSummary.js
+++ burgerapp/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,35 @@
-import React from 'react';
-import Aux from '../../../hoc/Auxiliary';
+import React, {Component} from 'react';
+import Aux from '../../../hoc/Auxiliary/Auxiliary';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
+class OrderSummary extends Component {
+    componentWillUpdate(){
+        console.log(`[OrderSummary] updated`);
+    }
 
-    const ingredientsSummary = Object.keys(props.ingredients)
-                                .map( igKey => {
-                                    return (
-                                    <li key={igKey}>
-                                        <span style={{textTransform:'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
-                                    </li>);
-                                });
-
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with following ingredients:</p>
-            <ul>
-                {ingredientsSummary}
-            </ul>
-    <p><strong>Total Price: {props.totalPrice.toFixed(2)} USD</strong></p>
-            <p>Continue to checkout?</p>
-            <Button btnType='Danger' clicked={props.cancel}>CANCEL</Button>
-            <Button btnType='Success' clicked={props.continue}>CONTINUE</Button>
-        </Aux>
-    );
+    render(){
+        const ingredientsSummary = Object.keys(this.props.ingredients).map( igKey => {
+            return (
+            <li key={igKey}>
+                <span style={{textTransform:'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
+            </li>);
+        });
+    
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with following ingredients:</p>
+                <ul>
+                    {ingredientsSummary}
+                </ul>
+        <p><strong>Total Price: {this.props.totalPrice.toFixed(2)} USD</strong></p>
+                <p>Continue to checkout?</p>
+                <Button btnType='Danger' clicked={this.props.cancel}>CANCEL</Button>
+                <Button btnType='Success' clicked={this.props.continue}>CONTINUE</Button>
+            </Aux>
+        );
+    }
 }
 
-export default orderSummary;
+
+export default OrderSummary;
--- burgerapp/src/components/UI/Modal/Modal.js
+++ burgerapp/src/components/UI/Modal/Modal.js
@@ -1,22 +1,32 @@
-import React  from 'react';
+import React, {Component}  from 'react';
 import classes from './Modal.css';
-import Aux from '../../../hoc/Auxiliary';
+import Aux from '../../../hoc/Auxiliary/Auxiliary';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => {
-    return (
-        <Aux>
-            <Backdrop show={props.show} clicked={props.cancelPurchaing} />
-            <div className={classes.Modal}
-                style={{
-                    transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                    opacity: props.show ? '1' : '0'
-                }}
-            >
-                {props.children}
-            </div>
-        </Aux>
-    );
+class Modal extends Component {
+    shouldComponentUpdate(nextProps, nextState){
+        return nextProps.show !== this.props.show;
+    }
+
+    componentWillUpdate(){
+        console.log('[Modal] updated');
+    }
+
+    render(){
+        return (
+            <Aux>
+                <Backdrop show={this.props.show} clicked={this.props.cancelPurchaing} />
+                <div className={classes.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }}
+                >
+                    {this.props.children}
+                </div>
+            </Aux>
+        );
+    }
 }
 
-export default modal;
+export default Modal;
