36a31bbbf73b1b34f8d1af96ecbb8cb7572a176e
Nguyen Thanh Nam <thanhnam192@gmail.com>
2020-05-31 15:18:34 +0700

    disable minimize css
    disable minimize css

--- burgerapp/config/webpack.config.prod.js
+++ burgerapp/config/webpack.config.prod.js
@@ -176,7 +176,7 @@ module.exports = {
                       loader: require.resolve('css-loader'),
                       options: {
                         importLoaders: 1,
-                        minimize: true,
+                        minimize: false,
                         sourceMap: shouldUseSourceMap,
                         modules: true,
                         localIdentName: '[name]__[local]__[hash:base64:5]'
